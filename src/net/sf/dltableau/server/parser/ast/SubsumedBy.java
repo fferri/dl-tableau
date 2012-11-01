d6aabdb1daa4013d69a2e1e039e63944927ec7d3
federico.ferri.it@gmail.com <federico.ferri.it@gmail.com@4ab758cd-fb69-1959-c117-28830cdfa4bd>
2012-11-01 12:49:26 +0000

    converion of subsumption to DNF

--- src/net/sf/dltableau/server/parser/ast/SubsumedBy.java
+++ src/net/sf/dltableau/server/parser/ast/SubsumedBy.java
@@ -4,4 +4,8 @@ public class SubsumedBy extends AbstractDefinition {
 	public SubsumedBy(Atom op1, AbstractNode op2) {
 		super(op1, op2);
 	}
+	
+	public Or asNormalForm() {
+		return new Or(new Not(op1), op2);
+	}
 }
