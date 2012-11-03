7f1a64d514ab31565dd946db6935df1466fd7dbc
federico.ferri.it@gmail.com <federico.ferri.it@gmail.com@4ab758cd-fb69-1959-c117-28830cdfa4bd>
2012-11-03 20:58:22 +0000

    add RPC method for new-style TBOX

--- src/net/sf/dltableau/client/DLTableauServiceAsync.java
+++ src/net/sf/dltableau/client/DLTableauServiceAsync.java
@@ -1,5 +1,7 @@
 package net.sf.dltableau.client;
 
+import java.util.List;
+
 import net.sf.dltableau.shared.DLTableauBean;
 import net.sf.dltableau.shared.DLTableauOptions;
 
@@ -9,7 +11,14 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
  * The async counterpart of <code>DLTableauService</code>.
  */
 public interface DLTableauServiceAsync {
+	void parseConceptString(String s, AsyncCallback<String> callback);
+	
+	void parseDefinitionString(String s, AsyncCallback<String> callback);
+
 	void solve(String formula, DLTableauOptions options, AsyncCallback<DLTableauBean> callback);
 
-	void syntaxTree(String formula, DLTableauOptions options, AsyncCallback<String> callback);
+	void syntaxTree(String concept, DLTableauOptions options, AsyncCallback<String> callback);
+
+	void solve(List<String> tboxDefs, String concept, DLTableauOptions options,
+			AsyncCallback<DLTableauBean> callback);
 }
