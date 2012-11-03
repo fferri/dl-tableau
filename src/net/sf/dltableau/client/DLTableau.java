72e8f489516fd0909b8e38922d436cebaf96b35a
federico.ferri.it@gmail.com <federico.ferri.it@gmail.com@4ab758cd-fb69-1959-c117-28830cdfa4bd>
2012-11-03 21:04:19 +0000

    new ui with TBOX

--- src/net/sf/dltableau/client/DLTableau.java
+++ src/net/sf/dltableau/client/DLTableau.java
@@ -1,5 +1,8 @@
 package net.sf.dltableau.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.sf.dltableau.shared.DLTableauBean;
 import net.sf.dltableau.shared.DLTableauOptions;
 
@@ -7,6 +10,8 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.DoubleClickEvent;
+import com.google.gwt.event.dom.client.DoubleClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
@@ -17,49 +22,137 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasEnabled;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
 
 public class DLTableau implements EntryPoint {
 	private final DLTableauServiceAsync tableauService = GWT.create(DLTableauService.class);
 	private final DLTableauOptions tableauOptions = new DLTableauOptions();
 	
+	final TextBox tboxEdit = new TextBox();
+	final Button tboxAddButton = new Button("Add");
+	final ListBox tboxField = new ListBox(true);
+	final Button tboxEditButton = new Button("Edit");
+	final Button tboxRemoveButton = new Button("Remove");
+	
+	final TextBox formulaField = new TextBox();
 	final Button goButton = new Button("Go");
 	final Button astButton = new Button("AST");
-	final TextArea tboxField = new TextArea();
-	final TextBox formulaField = new TextBox();
+	
+	final CheckBox useUnicode = new CheckBox("Use UNICODE symbols");
 	final Label errorLabel = new Label();
 	final HTML outputLabel = new HTML();
-	final CheckBox useUnicode = new CheckBox("Use UNICODE symbols");
+	
+	final HasEnabled lockableUiItems[] = new HasEnabled[]{
+		tboxEdit, tboxAddButton,
+		tboxField, tboxEditButton, tboxRemoveButton,
+		formulaField, goButton, astButton,
+		useUnicode
+	};
 
 	public void onModuleLoad() {
-		tboxField.setText("D subsumed-by exists R. C;\nC = D;");
-		formulaField.setText("exists R. D and forall R. (not C)");
-		
+		addTBOXDef("D subsumed-by exists R. C");
+		addTBOXDef("C = D");
+		setConceptString("exists R. D and forall R. (not C)");
+				
 		tboxField.addStyleName("tbox");
+		tboxField.addStyleName("formula");
+		tboxEdit.addStyleName("formula");
 		formulaField.addStyleName("formula");
+
+		tboxAddButton.addStyleName("button");
+		tboxEditButton.addStyleName("button");
+		tboxRemoveButton.addStyleName("button");
+		
 		goButton.addStyleName("button");
 		astButton.addStyleName("button");
 
 		// Use RootPanel.get() to get the entire body element
+		RootPanel.get("tboxEditorContainer").add(tboxEdit);
+		RootPanel.get("tboxAddButtonContainer").add(tboxAddButton);
 		RootPanel.get("tboxFieldContainer").add(tboxField);
-		RootPanel.get("nameFieldContainer").add(formulaField);
+		RootPanel.get("tboxEditButtonContainer").add(tboxEditButton);
+		RootPanel.get("tboxRemoveButtonContainer").add(tboxRemoveButton);
+		
+		RootPanel.get("conceptFieldContainer").add(formulaField);
 		RootPanel.get("sendButtonContainer").add(goButton);
+		RootPanel.get("astButtonContainer").add(astButton);
+		
+		RootPanel.get("optionsContainer").add(useUnicode);
+		
 		RootPanel.get("errorLabelContainer").add(errorLabel);
 		RootPanel.get("output").add(outputLabel);
-		RootPanel.get("astButtonContainer").add(astButton);
-		RootPanel.get("optionsContainer").add(useUnicode);
 
 		useUnicode.setValue(tableauOptions.isUseUnicodeSymbols());
 		
 		formulaField.setFocus(true);
 		formulaField.selectAll();
 		
-		useUnicode.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
-			public void onValueChange(ValueChangeEvent<Boolean> event) {
-				tableauOptions.setUseUnicodeSymbols(event.getValue());
+		tboxEdit.addKeyUpHandler(new KeyUpHandler() {
+			public void onKeyUp(KeyUpEvent event) {
+				if(event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+					addTBOXDef(tboxEdit.getText());
+					tboxEdit.setText("");
+				}
+			}
+		});
+		
+		tboxAddButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				addTBOXDef(tboxEdit.getText());
+				tboxEdit.setText("");
+			}
+		});
+		
+		tboxField.addDoubleClickHandler(new DoubleClickHandler() {
+			public void onDoubleClick(DoubleClickEvent event) {
+				int i = tboxField.getSelectedIndex();
+				if(i >= 0) {
+					tboxEdit.setText(tboxField.getItemText(i));
+					tboxField.removeItem(i);
+				}
+			}
+		});
+		
+		tboxField.addKeyUpHandler(new KeyUpHandler() {
+			public void onKeyUp(KeyUpEvent event) {
+				if(event.getNativeKeyCode() == KeyCodes.KEY_BACKSPACE ||
+						event.getNativeKeyCode() == KeyCodes.KEY_DELETE) {
+					int i = tboxField.getSelectedIndex();
+					if(i >= 0) {
+						tboxField.removeItem(i);
+					}
+				}
+			}
+		});
+		
+		tboxEditButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				int i = tboxField.getSelectedIndex();
+				if(i >= 0) {
+					tboxEdit.setText(tboxField.getItemText(i));
+					tboxField.removeItem(i);
+				}
+			}
+		});
+		
+		tboxRemoveButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				int i = tboxField.getSelectedIndex();
+				if(i >= 0) {
+					tboxField.removeItem(i);
+				}
+			}
+		});
+		
+		formulaField.addKeyUpHandler(new KeyUpHandler() {
+			public void onKeyUp(KeyUpEvent event) {
+				if(event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+					computeTableau();
+				}
 			}
 		});
 
@@ -69,34 +162,47 @@ public class DLTableau implements EntryPoint {
 			}
 		});
 		
-		formulaField.addKeyUpHandler(new KeyUpHandler() {
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER)
-					computeTableau();
-			}
-		});
-		
 		astButton.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
 				computeSyntaxTree();
 			}
 		});
+		
+		useUnicode.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
+			public void onValueChange(ValueChangeEvent<Boolean> event) {
+				tableauOptions.setUseUnicodeSymbols(event.getValue());
+			}
+		});
+	}
+	
+	private void addTBOXDef(String s) {
+		tboxField.addItem(s.trim());
+	}
+	
+	private void setConceptString(String s) {
+		formulaField.setText(s.trim());
 	}
 	
 	private void lockUi(boolean lock) {
-		goButton.setEnabled(!lock);
-		astButton.setEnabled(!lock);
-		formulaField.setEnabled(!lock);
-		useUnicode.setEnabled(!lock);
+		for(HasEnabled item : lockableUiItems) item.setEnabled(!lock);
+	}
+	
+	private List<String> getTBOXDefsStringList() {
+		List<String> tboxDefs = new ArrayList<String>();
+		for(int i = 0; i < tboxField.getItemCount(); i++) tboxDefs.add(tboxField.getItemText(i));
+		return tboxDefs;
+	}
+	
+	private String getConceptString() {
+		return formulaField.getText();
 	}
 
 	private void computeTableau() {
 		errorLabel.setText("");
 		lockUi(true);
 		tableauService.solve(
-			tboxField.getText() + formulaField.getText(),
-			tableauOptions,
+			getTBOXDefsStringList(), getConceptString(), tableauOptions,
 			new AsyncCallback<DLTableauBean>() {
 				public void onFailure(Throwable e) {
 					Window.alert("Remote Procedure Call - Failure:\n\n" + e.getMessage());
@@ -114,7 +220,7 @@ public class DLTableau implements EntryPoint {
 	private void computeSyntaxTree() {
 		lockUi(true);
 		tableauService.syntaxTree(
-			formulaField.getText(),
+			getConceptString(),
 			tableauOptions,
 			new AsyncCallback<String>() {
 				public void onFailure(Throwable e) {
