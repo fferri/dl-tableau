7eeb90f869e4211a1e2c8f7a705df31d6077dcc1
federico.ferri.it@gmail.com <federico.ferri.it@gmail.com@4ab758cd-fb69-1959-c117-28830cdfa4bd>
2012-11-08 14:22:44 +0000

    changes for interactive solver

--- src/net/sf/dltableau/shared/DLTableauNode.java
+++ src/net/sf/dltableau/shared/DLTableauNode.java
@@ -6,6 +6,6 @@ import java.util.List;
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 public class DLTableauNode implements IsSerializable {
-	public List<String> expr = new ArrayList<String>();
+	public List<DLTableauInstance> expr = new ArrayList<DLTableauInstance>();
 	public List<DLTableauNode> child = new ArrayList<DLTableauNode>();
 }
